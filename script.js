5c811c312c38e451a289ab1887f46df9d9de8b12
marleypm16 <[200~rmcctec@proton.me>
2024-01-08 18:36:30 -0300

    first commit feat: addCaracteres,cleanContas on going : result

---
+++ script.js
@@ -0,0 +1,44 @@
+const contas = document.getElementById("contas");
+const resultado = document.getElementById("resultado");
+const operadores = document.querySelectorAll(".operadores")
+const numeros = document.querySelectorAll('.numeros');
+const clear = document.getElementById("clean");
+
+const result = (conta) =>{
+    const resultadoCalculado = eval(conta);
+
+    // Certifique-se de que o resultado é um número antes de exibi-lo
+    if (!isNaN(resultadoCalculado)) {
+        conta = resultadoCalculado;
+    } else {
+        // Se a expressão não for válida, você pode tratar isso de acordo
+        conta = 'Erro';
+    }
+}
+
+const cleanContas = () => {
+    contas.innerHTML = '';
+};
+const addInContas = (element) => {
+    contas.innerHTML += element;
+};
+
+
+for (const numero of numeros) {
+    numero.addEventListener("click", () => {
+        addInContas(numero.innerHTML);
+    });
+}
+for (const operador of operadores) {
+    operador.addEventListener("click", () => {
+        addInContas(operador.innerHTML);
+    });
+}
+
+
+
+clear.addEventListener("click", cleanContas);
+
+resultado.addEventListener("click", () => {
+    result(contas.innerHTML)
+});
