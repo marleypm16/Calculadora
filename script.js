bea3351536392d884a07956a32eec95f808753b0
marleypm16 <[200~rmcctec@proton.me>
2024-01-09 10:41:34 -0300

    feat: del

--- script.js
+++ script.js
@@ -1,52 +1,58 @@
 const contas = document.getElementById("contas");
 const resultado = document.getElementById("resultado");
-const operadores = document.querySelectorAll(".operadores")
-const numeros = document.querySelectorAll('.numeros');
+const operadores = document.querySelectorAll(".operadores");
+const numeros = document.querySelectorAll(".numeros");
 const clear = document.getElementById("clean");
-let ultimoCaractere = ''
+const deletar = document.querySelector(".deletar");
+let ultimoCaractere = "";
 
 const result = () => {
-    const resultadoCalculado = eval(contas.innerHTML);
+  const resultadoCalculado = eval(contas.innerHTML);
 
-    // Certifique-se de que o resultado é um número antes de exibi-lo
-    if (!isNaN(resultadoCalculado)) {
-        contas.innerHTML = resultadoCalculado;
-    } else {
-        // Se a expressão não for válida, você pode tratar isso de acordo
-        contas.innerHTML = 'Erro';
-    }
-}
+  // Certifique-se de que o resultado é um número antes de exibi-lo
+  if (!isNaN(resultadoCalculado)) {
+    contas.innerHTML = resultadoCalculado;
+  } else {
+    // Se a expressão não for válida, você pode tratar isso de acordo
+    contas.innerHTML = "Erro";
+  }
+};
 
 const cleanContas = () => {
-    contas.innerHTML = '';
-    ultimoCaractere = ''
+  contas.innerHTML = "";
+  ultimoCaractere = "";
 };
 const addInContas = (element) => {
-    // Verifica se o último caractere foi um operador e o novo elemento é um operador
-    if ((isNaN(ultimoCaractere) && isNaN(element))) {
-        return;
-    }
+  // Verifica se o último caractere foi um operador e o novo elemento é um operador
+  if (isNaN(ultimoCaractere) && isNaN(element)) {
+    return;
+  }
 
-    contas.innerHTML += element;
-    ultimoCaractere = element;
+  contas.innerHTML += element;
+  ultimoCaractere = element;
 };
 
+const deleteLastCaractere = () => {
+  const excludelastElement = contas.innerHTML.slice(0, -1);
+  contas.innerHTML = excludelastElement;
+};
 
 for (const numero of numeros) {
-    numero.addEventListener("click", () => {
-        addInContas(numero.innerHTML);
-    });
+  numero.addEventListener("click", () => {
+    addInContas(numero.innerHTML);
+  });
 }
 for (const operador of operadores) {
-    operador.addEventListener("click", () => {
-        addInContas(operador.innerHTML);
-    });
+  operador.addEventListener("click", () => {
+    addInContas(operador.innerHTML);
+  });
 }
-
-
 
 clear.addEventListener("click", cleanContas);
 
 resultado.addEventListener("click", () => {
-    result()
+  result();
+});
+deletar.addEventListener("click", () => {
+  deleteLastCaractere();
 });
