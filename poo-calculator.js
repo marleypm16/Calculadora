209171aa6b02c3533d7882280e7a145fa56ad682
marleypm16 <[200~rmcctec@proton.me>
2024-01-09 18:23:35 -0300

    poo calculadora

---
+++ poo-calculator.js
@@ -0,0 +1,63 @@
+const contas = document.getElementById("contas");
+const resultado = document.getElementById("resultado");
+const operadores = document.querySelectorAll(".operadores");
+const numeros = document.querySelectorAll(".numeros");
+const clear = document.getElementById("clean");
+const deletar = document.querySelector(".deletar");
+var ultimoCaractere = ''
+
+class Calculadora{
+
+    addInContas(element){
+        if(isNaN(element) && isNaN(ultimoCaractere)){
+            return
+        }
+
+        contas.innerHTML += element
+        ultimoCaractere = element
+    }
+
+    result(){
+        const resultadoCalculado = eval(contas.innerHTML);
+
+        // Certifique-se de que o resultado é um número antes de exibi-lo
+        if (!isNaN(resultadoCalculado)) {
+          contas.innerHTML = resultadoCalculado;
+        } else {
+          // Se a expressão não for válida, você pode tratar isso de acordo
+          contas.innerHTML = "Erro";
+        }
+    }
+
+    clean(){
+        contas.innerHTML = "";
+        ultimoCaractere = "";
+    }
+
+    deletaCaractere(){
+        const excludelastElement = contas.innerHTML.slice(0, -1);
+        contas.innerHTML = excludelastElement;
+    }
+}
+
+const calculadora = new Calculadora()
+
+for (const numero of numeros) {
+    numero.addEventListener("click", () => {
+      calculadora.addInContas(numero.innerHTML);
+    });
+  }
+  for (const operador of operadores) {
+    operador.addEventListener("click", () => {
+      calculadora.addInContas(operador.innerHTML);
+    });
+  }
+
+clear.addEventListener("click", calculadora.clean);
+
+resultado.addEventListener("click", () => {
+  calculadora.result();
+});
+deletar.addEventListener("click", () => {
+  calculadora.deletaCaractere
+});
