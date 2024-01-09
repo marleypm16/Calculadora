4ea63b829ad81c564144779a84c50f2e4548fcbb
marleypm16 <[200~rmcctec@proton.me>
2024-01-09 18:39:14 -0300

    progress

--- script.js
+++ script.js
@@ -4,55 +4,60 @@ const operadores = document.querySelectorAll(".operadores");
 const numeros = document.querySelectorAll(".numeros");
 const clear = document.querySelector(".limpar");
 const deletar = document.querySelector(".deletar");
-let ultimoCaractere = "";
+var ultimoCaractere = ''
 
-const result = () => {
-  const resultadoCalculado = eval(contas.innerHTML);
+class Calculadora{
 
-  // Certifique-se de que o resultado é um número antes de exibi-lo
-  if (!isNaN(resultadoCalculado)) {
-    contas.innerHTML = resultadoCalculado;
-  } else {
-    // Se a expressão não for válida, você pode tratar isso de acordo
-    contas.innerHTML = "Erro";
-  }
-};
+    addInContas(element){
+        if(isNaN(element) && isNaN(ultimoCaractere)){
+            return
+        }
 
-const cleanContas = () => {
-  contas.innerHTML = "";
-  ultimoCaractere = "";
-};
-const addInContas = (element) => {
-  // Verifica se o último caractere foi um operador e o novo elemento é um operador
-  if (isNaN(ultimoCaractere) && isNaN(element)) {
-    return;
-  }
+        contas.innerHTML += element
+        ultimoCaractere = element
+    }
 
-  contas.innerHTML += element;
-  ultimoCaractere = element;
-};
+    result(){
+        const resultadoCalculado = eval(contas.innerHTML);
 
-const deleteLastCaractere = () => {
-  const excludelastElement = contas.innerHTML.slice(0, -1);
-  contas.innerHTML = excludelastElement;
-};
+        // Certifique-se de que o resultado é um número antes de exibi-lo
+        if (!isNaN(resultadoCalculado)) {
+          contas.innerHTML = resultadoCalculado;
+        } else {
+          // Se a expressão não for válida, você pode tratar isso de acordo
+          contas.innerHTML = "Erro";
+        }
+    }
+
+    clean(){
+        contas.innerHTML = "";
+        ultimoCaractere = "";
+    }
+
+    deletaCaractere(){
+        const excludelastElement = contas.innerHTML.slice(0, -1);
+        contas.innerHTML = excludelastElement;
+    }
+}
+
+const calculadora = new Calculadora()
 
 for (const numero of numeros) {
-  numero.addEventListener("click", () => {
-    addInContas(numero.innerHTML);
-  });
-}
-for (const operador of operadores) {
-  operador.addEventListener("click", () => {
-    addInContas(operador.innerHTML);
-  });
-}
+    numero.addEventListener("click", () => {
+      calculadora.addInContas(numero.innerHTML);
+    });
+  }
+  for (const operador of operadores) {
+    operador.addEventListener("click", () => {
+      calculadora.addInContas(operador.innerHTML);
+    });
+  }
 
-clear.addEventListener("click", cleanContas);
+clear.addEventListener("click", calculadora.clean);
 
 resultado.addEventListener("click", () => {
-  result();
+  calculadora.result();
 });
 deletar.addEventListener("click", () => {
-  deleteLastCaractere();
+  calculadora.deletaCaractere
 });
